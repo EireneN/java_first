096ac80eabf98ea56ef83c237d1ddcdae08c7462
Nosova Irina <NosovaEirene@yandex.ru>
2022-05-01 17:34:58 +0300

    Задание 2 (версия 1.1)

--- sandbox/src/main/java/irina/jft/sandbox/Main.java
+++ sandbox/src/main/java/irina/jft/sandbox/Main.java
@@ -5,44 +5,11 @@ public class Main {
         Point p1 = new Point(3, 2);
         Point p2 = new Point(5,6);
 
-        printCoordinates (p1);
-        printCoordinates (p2);
+        p1.printCoordinates (p1);
+        p2.printCoordinates (p2);
 
-        distance(p1, p2);
+        p1.distance (p1, p2);
 
-    }
-    public static void distance (Point p1, Point p2){
-        double bc = bc(p1, p2);
-        double ac = ac(p1, p2);
-        double sum = sum (ac, bc);
-        square(sum);
-    }
-
-    public static double bc (Point p1, Point p2){
-        double bc = (p2.x - p1.x) * (p2.x - p1.x);
-        System.out.println("BC:" + " = " + bc);
-        return bc;
-    }
-    public static double ac (Point p1, Point p2){
-        double ac = (p2.y - p1.y) * (p2.y - p1.y);
-        System.out.println("AC:" + " = " + ac);
-        return ac;
-    }
-
-    public static double sum (double ac, double bc){
-        double sum = ac + bc;
-        System.out.println("Sum AC + BC = " + sum);
-        return sum;
-    }
-    public static double square (double sum){
-        double distance = Math.sqrt(sum);
-        System.out.println("Distance = " + distance);
-        return distance;
-    }
-
-
-    public static void printCoordinates (Point a) {
-    System.out.println("Координаты точки:" + " x = " + a.x + ", " + "y = " + a.y + "." );
     }
 
 }
--- sandbox/src/main/java/irina/jft/sandbox/Point.java
+++ sandbox/src/main/java/irina/jft/sandbox/Point.java
@@ -8,6 +8,39 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+    public void distance (Point p1, Point p2){
+        double bc = bc(p1, p2);
+        double ac = ac(p1, p2);
+        double sum = sum (ac, bc);
+        square(sum);
+    }
+
+    public double bc (Point p1, Point p2){
+        double bc = (p2.x - p1.x) * (p2.x - p1.x);
+        System.out.println("BC:" + " = " + bc);
+        return bc;
+    }
+    public double ac (Point p1, Point p2){
+        double ac = (p2.y - p1.y) * (p2.y - p1.y);
+        System.out.println("AC:" + " = " + ac);
+        return ac;
+    }
+
+    public double sum (double ac, double bc){
+        double sum = ac + bc;
+        System.out.println("Sum AC + BC = " + sum);
+        return sum;
+    }
+    public void square (double sum){
+        double square = Math.sqrt(sum);
+        System.out.println("Distance = " + square);
+    }
+
+
+    public void printCoordinates (Point a) {
+        System.out.println("Координаты точки:" + " x = " + a.x + ", " + "y = " + a.y + "." );
+    }
+
 }
 
 
