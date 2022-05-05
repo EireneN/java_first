f75302e741b4f7641f120e3062fd495b448b34bf
Nosova Irina <NosovaEirene@yandex.ru>
2022-05-06 01:18:23 +0300

    Обновление фикстуры

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContact.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,19 +13,13 @@ public class NewContact {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/index.php");
+    login();
   }
 
   @Test
   public void testNewContact() throws Exception {
     wd.get("http://localhost/addressbook/index.php");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.xpath("//form[@id='LoginForm']/label[2]")).click();
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
     wd.findElement(By.linkText("add new")).click();
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
@@ -41,13 +36,28 @@ public class NewContact {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     wd.findElement(By.linkText("home page")).click();
     wd.findElement(By.linkText("Logout")).click();
+
+  }
+
+  private void login() {
+    wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.xpath("//form[@id='LoginForm']/label[2]")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
+    logout();
     wd.quit();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
   }
 
   private boolean isElementPresent(By by) {
