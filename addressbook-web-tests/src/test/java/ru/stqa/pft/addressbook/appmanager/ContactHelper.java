0622f45409a7fdabc7e8693bf96f1320c92d36ec
Nosova Irina <NosovaEirene@yandex.ru>
2022-05-07 23:41:23 +0300

    Трансформация NewContactTests (двухуровневая архитектура)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase{
+
+    public ContactHelper (WebDriver wd) {
+        super(wd);
+    }
+
+    public void initContactCreation() {
+        click(By.linkText("add new"));
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        type(By.name("firstname"), contactData.getContactName());
+        type(By.name("lastname"), contactData.getContactSurname());
+        type(By.name("mobile"), contactData.getContactMobNumber());
+        type(By.name("email"), contactData.getContactEmail());
+    }
+    public void SendForm() {
+        click(By.xpath("//div[@id='content']/form/input[21]"));
+    }
+}
+
