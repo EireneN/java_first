2436d4fbe103829caee872937932292a4c6708a3
Nosova Irina <NosovaEirene@yandex.ru>
2022-05-18 22:06:14 +0300

    Ускорение работы тестов по вычислению Primes (урок 4.1)

--- sandbox/src/test/java/irina/jft/sandbox/PrimeTests.java
+++ sandbox/src/test/java/irina/jft/sandbox/PrimeTests.java
@@ -7,9 +7,14 @@ public class PrimeTests {
 
     @Test
     public void testsPrime() {
-        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
     }
 
+    @Test (enabled = false)
+    public void testPrimeLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
     @Test
     public void testNOPrime() {
         Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
