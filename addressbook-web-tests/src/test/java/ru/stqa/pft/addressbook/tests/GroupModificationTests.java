280aca2dc493d570ebea46e8bab37c99090cb741
Nosova Irina <NosovaEirene@yandex.ru>
2022-06-12 18:46:44 +0300

    Лекция 7.5. Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -40,5 +40,7 @@ public class GroupModificationTests extends TestBase {
             assertThat(app.group().count(), equalTo(before.size()));
             Groups after = app.db().groups();
             assertThat(after, CoreMatchers.equalTo(before.without(modifiedGroup).withAdded(group)));
+            verifyGroupListInUI();
         }
+
 }
